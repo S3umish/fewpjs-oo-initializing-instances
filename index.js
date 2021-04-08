e9a562db35b726a1e5a73673d6683e061997d61a
sunitee <suniteebajpai2@gmail.com>
2021-04-07 20:01:45 -0400

    commit tree ba8496e70cda7b4609c3e9ec8225fdc59558d590

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code here
+
+class Breakfast {
+   constructor(food, drink) {
+      this.food = food
+      this.drink = drink 
+   } 
+}
+
+let firstMeal = new Breakfast("eggs", "juice")
+
+class Lunch {
+    constructor(salad, soup, drink){
+        this.salad = salad
+        this.soup = soup
+        this.drink = drink
+    }
+}
+
+let secondMeal = new Lunch("caesar salad", "brocolli cheddar soup", "iced tea")
+
+
+class Dinner {
+    constructor(salad, soup, entree, dessert){
+        this.salad = salad
+        this.soup = soup
+        this.entree = entree
+        this._dessert = dessert
+    }
+}
+
+let finalMeal = new Dinner("balsamic salad", "consomme", "filet mignon", "cheesecake")
+
